b7f55b3dc2d22547f1ad2257fff80934a402e7b7
Lee Zhang <leezhangsc@gmail.com>
2025-06-16 15:02:32 +0200

    Add in redirect so we only have a 1 page site

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -5,15 +5,19 @@ const AppError = require('../utils/appError');
 const Booking = require('../models/bookingModel');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from collection
-  const tours = await Tour.find();
+  // // 1) Get tour data from collection
+  // const tours = await Tour.find();
 
-  // 2) Build template
-  // 3) Render that template using tour data from 1)
-  res.status(200).render('overview', {
-    title: 'All Tours',
-    tours,
-  });
+  // // 2) Build template
+  // // 3) Render that template using tour data from 1)
+  // res.status(200).render('overview', {
+  //   title: 'All Tours',
+  //   tours,
+  // });
+
+  // temp simplification
+
+  res.redirect('/tour/high-in-the-city-of-love');
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
